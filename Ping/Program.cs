1039ce0562fe2183d99b50fa374f17ba006d4320
Paulo Rogério Martins Marques <paulo.analista@outlook.com>
2019-01-15 16:53:10 -0200

    Add

---
+++ Ping/Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using prmToolkit.Selenium;
+using prmToolkit.Selenium.Enum;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ping
+{
+    class Program
+    {
+        //private static AutoResetEvent waitHandle = new AutoResetEvent(false);
+        static void Main(string[] args)
+        {
+            var capability = DesiredCapabilities.Chrome();
+            IWebDriver _driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub/"), capability, TimeSpan.FromSeconds(600));
+
+            _driver.Navigate().GoToUrl("http://ilovecode.com.br");
+
+            _driver.Quit();
+
+            //waitHandle.WaitOne();
+        }
+    }
+}
